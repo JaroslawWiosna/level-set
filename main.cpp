480bd97004dccebf78ffb2d5476a0e521bb7f0d5
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2016-12-16 20:19:19 +0100

    Parameters changed

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@ float Average_c1(float ** phi, float ** fimage, int rows, int cols)
 {
     float sum = 0;  //Sum of pixel values
     int pixnum = 0; //number of pixels inside circle
-    for (std::size_t i = 0; i < rows; ++i)
-        for (std::size_t j = 0; j < cols; ++j)
+    for (std::size_t i = 1; i < rows-1; ++i)
+        for (std::size_t j = 1; j < cols-1; ++j)
         {
             if (Heaviside(phi[i][j]))   //if phi > 0 ==> pixel inside circle
             {
@@ -30,8 +30,8 @@ float Average_c2(float ** phi, float ** fimage, int rows, int cols)
 {
     float sum = 0;  //Sum of pixel values
     int pixnum = 0; //number of pixels outside circle
-    for (std::size_t i = 0; i < rows; ++i)
-        for (std::size_t j = 0; j < cols; ++j)
+    for (std::size_t i = 1; i < rows-1; ++i)
+        for (std::size_t j = 1; j < cols-1; ++j)
         {
             if (!Heaviside(phi[i][j]))  //if phi < 0 ==> pixel inside circle
             {
@@ -83,24 +83,27 @@ int main(int argc, char** argv )
             if (100 - sqrt( (rows/2-i)*(rows/2-i) + (cols/2-j)*(cols/2-j) ) >= 0)
                 phi[i][j] = 1;
             else
-                phi[i][j] = -1;*/
+                phi[i][j] = -1;
+		*/
             phi[i][j] = sin(3.1415*i/5)*sin(3.1415*j/5);
 	}
 
-
-    for (std::size_t step = 0; step < 50; ++step)
+    bool should_continue = true;
+    for (std::size_t step = 0; step < 500 && should_continue; ++step)
     {
+	should_continue = false;
         float c1 = Average_c1(phi, fimage, rows, cols);
         float c2 = Average_c2(phi, fimage, rows, cols);;
         for (std::size_t i = 1; i < rows-1; ++i)
             for (std::size_t j = 1; j < cols-1; ++j)
             {
-	        float mi = 0.00001;
-		float eta = 0.0000001;
-		float ni = 0.0001;
-		float lambda1 = 0.0001;
-		float lambda2 = 0.0001;
+	        float mi = 0.3;
+		float eta = 0.00000001;
+		float ni = 0.00000001;
+		float lambda1 = 0.001;
+		float lambda2 = 0.001;
 		float eps = 0.0001;
+		float dt = 1; // timestep
 		
 	        //float A = mi / (sqrt( pow(eta,2) + pow((phi[i+1][j] - phi[i][j]),2)  + pow((phi[i][j+1] - phi[i][j-1])/2.0,2) ));
 	        //float B = mi / (sqrt( pow(eta,2) + pow((phi[i+1][j] - phi[i-1][j]/2.0),2)  + pow((phi[i][j] - phi[i+1][j])/2.0,2) ));
@@ -109,18 +112,29 @@ int main(int argc, char** argv )
                 auto A = [](int i, int j, float ** phi, float & mi, float & eta)->float { return mi / (sqrt( pow(eta,2) + pow((phi[i+1][j] - phi[i][j]),2)  + pow((phi[i][j+1] - phi[i][j-1])/2.0,2) ));};
                 auto B = [](int i, int j, float ** phi, float & mi, float & eta)->float { return mi / (sqrt( pow(eta,2) + pow((phi[i+1][j] - phi[i-1][j]/2.0),2)  + pow((phi[i][j] - phi[i+1][j])/2.0,2) ));};
                 
-	        phi[i][j] = (phi[i][j] + (eps/ (3.1415 * (pow(eps,2) + pow(phi[i][j],2)))) * A(i,j,phi,mi,eta)* phi[i+1][j] + A(i-1,j,phi,mi,eta)*phi[i-1][j] + B(i,j,phi,mi,eta)*phi[i][j+1] + B(i,j-1,phi,mi,eta)*phi[i][j-1] - ni - lambda1*pow(fimage[i][j] - c1,2)   - lambda2*pow(fimage[i][j] - c2,2) ) / ( 1 + (eps/ (3.1415 * (pow(eps,2) + pow(phi[i][j],2)))) * (A(i,j,phi,mi,eta) + A(i-1,j,phi,mi,eta) + B(i,j,phi,mi,eta) + B(i,j-1,phi,mi,eta))  ) ;
+		auto old_value = phi[i][j];
+
+	        phi[i][j] = (phi[i][j] + dt*(eps/ (3.1415 * (pow(eps,2) + pow(phi[i][j],2)))) * A(i,j,phi,mi,eta)* phi[i+1][j] + A(i-1,j,phi,mi,eta)*phi[i-1][j] + B(i,j,phi,mi,eta)*phi[i][j+1] + B(i,j-1,phi,mi,eta)*phi[i][j-1] - ni - lambda1*pow(fimage[i][j] - c1,2)   - lambda2*pow(fimage[i][j] - c2,2) )
+		       	/ 
+			( 1 + dt*(eps/ (3.1415 * (pow(eps,2) + pow(phi[i][j],2)))) * (A(i,j,phi,mi,eta) + A(i-1,j,phi,mi,eta) + B(i,j,phi,mi,eta) + B(i,j-1,phi,mi,eta))  ) ;
+                if ( phi[i][j] - old_value > 0.001 ) 
+                {
+		        should_continue = true;
+	        }
 	    }
 	    cv::Mat3b rec(image.rows, image.cols, cv::Vec3b(0,0,0));
             for (std::size_t i = 0; i < rows; ++i)
                 for (std::size_t j = 0; j < cols; ++j)
                 {
-                    if(std::abs(phi[i][j]) < 0.1)
+                    if(phi[i][j] < 0)
+                        rec[i][j] = cv::Vec3b(0,0,255);
+                    if(phi[i][j] > 0)
+                        rec[i][j] = cv::Vec3b(155,0,0);
+                    if(std::abs(phi[i][j]) < 1)
                         rec[i][j] = cv::Vec3b(0,255,0);
                 }
                 cv::imshow("Display Image", rec);
                 cv::waitKey(0);
-            
     }
 
     
