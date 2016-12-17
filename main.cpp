68bf0acda1d816eeaeec2cd3be1f866da5115260
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2016-12-17 22:50:14 +0100

    Minor changes that makes it somehow working for 006.jpg

--- main.cpp
+++ main.cpp
@@ -44,7 +44,7 @@ int main(int argc, char** argv )
             else
                 phi[i][j] = -1;
 		*/
-            phi[i][j] = sin(3.1415*i/5)*sin(3.1415*j/5);
+            phi[i][j] = 0.02 * sin(3.1415*i/50)*sin(3.1415*j/50);
 	}
 
     bool should_continue = true;
@@ -57,13 +57,13 @@ int main(int argc, char** argv )
         for (std::size_t i = 1; i < rows-1; ++i)
             for (std::size_t j = 1; j < cols-1; ++j)
             {
-	        float mi = 0.3;
+	        float mi = 0.2;
 		float eta = 0.00000001;
-		float ni = 0.00000001;
-		float lambda1 = 0.001;
-		float lambda2 = 0.001;
-		float eps = 0.0001;
-		float dt = 1; // timestep
+		float ni = 0;//0.00000001;
+		float lambda1 = 0.300002;
+		float lambda2 = 0.300002;
+		float eps = 1000;
+		float dt = 0.001; // timestep
 		
                 auto A = 
                     [](int i, int j, float ** phi, float & mi, float & eta)
@@ -98,7 +98,7 @@ int main(int argc, char** argv )
 		        * (A(i,j,phi,mi,eta) + A(i-1,j,phi,mi,eta) 
                         + B(i,j,phi,mi,eta) + B(i,j-1,phi,mi,eta))  ) ;
 
-                if ( phi[i][j] - old_value > 0.001 ) 
+                if ( phi[i][j] - old_value > 0.0000001 ) 
                 {
 		        should_continue = true;
 	        }
@@ -107,11 +107,17 @@ int main(int argc, char** argv )
             for (std::size_t i = 0; i < rows; ++i)
                 for (std::size_t j = 0; j < cols; ++j)
                 {
-                    if(phi[i][j] < 0)
-                        rec[i][j] = cv::Vec3b(0,0,255);
-                    if(phi[i][j] > 0)
-                        rec[i][j] = cv::Vec3b(155,0,0);
+//                    if(phi[i][j] < 0)
+//                        rec[i][j] = cv::Vec3b(0,0,155);
+//                    if(phi[i][j] > 0)
+//                        rec[i][j] = cv::Vec3b(155,0,0);
+                    if(std::abs(phi[i][j]) < 100)
+                        rec[i][j] = cv::Vec3b(0,105,0);
+                    if(std::abs(phi[i][j]) < 10)
+                        rec[i][j] = cv::Vec3b(0,155,0);
                     if(std::abs(phi[i][j]) < 1)
+                        rec[i][j] = cv::Vec3b(0,205,0);
+                    if(std::abs(phi[i][j]) < 0.1)
                         rec[i][j] = cv::Vec3b(0,255,0);
                 }
                 cv::imshow("Display Image", rec);
