493ed3b5f132f71f8fcd349b768c511dc38e923e
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2016-12-22 10:30:04 +0100

    detectBorders(...) introduced

--- main.cpp
+++ main.cpp
@@ -30,64 +30,8 @@ int main(int argc, char** argv )
 
     initializePhi(phi, rows, cols);
 
-    bool should_continue = true;
-    for (std::size_t step = 0; step < 500 && should_continue; ++step)
-    {
-	//should_continue = false;
-        auto c = Average_c(phi, fimage, rows, cols);
-	auto c1 = c.first;
-	auto c2 = c.second;
-        for (std::size_t i = 1; i < rows-1; ++i)
-            for (std::size_t j = 1; j < cols-1; ++j)
-            {
-	        float mi = 0.2;
-		float eta = 0.00000001;
-		float ni = 0;//0.00000001;
-		float lambda1 = 0.300002;
-		float lambda2 = 0.300002;
-		float eps = 1000;
-		float dt = 0.001; // timestep
-		
-                auto A = 
-                    [](int i, int j, float ** phi, float & mi, float & eta)
-		    -> float { 
-                        return mi / (sqrt( pow(eta,2) 
-			    + pow((phi[i+1][j] - phi[i][j]),2) 
-			    + pow((phi[i][j+1] - phi[i][j-1])/2.0,2) ));
-		    };
-                auto B = 
-                    [](int i, int j, float ** phi, float & mi, float & eta)
-		    -> float { 
-                        return mi / (sqrt( pow(eta,2) 
-                            + pow((phi[i+1][j] - phi[i-1][j]/2.0),2) 
-			    + pow((phi[i][j] - phi[i+1][j])/2.0,2) ));
-		    };
-                
-		auto old_value = phi[i][j];
+    detectBorders(fimage, phi, rows, cols);
 
-	        phi[i][j] = 
-                    ( phi[i][j] 
-		        + dt * (eps/( M_PI * (pow(eps,2) + pow(phi[i][j],2))))
-		        * A(i,j,phi,mi,eta) *  phi[i+1][j] 
-			+ A(i-1,j,phi,mi,eta) * phi[i-1][j] 
-			+ B(i,j,phi,mi,eta) * phi[i][j+1] 
-			+ B(i,j-1,phi,mi,eta) * phi[i][j-1] 
-			- ni 
-			- lambda1 * pow(fimage[i][j] - c1,2) 
-			- lambda2*pow(fimage[i][j] - c2,2) )
-                    / 
-                    ( 1 + dt 
-		        * (eps / ( M_PI * ( pow(eps,2) + pow( phi[i][j],2) ))) 
-		        * (A(i,j,phi,mi,eta) + A(i-1,j,phi,mi,eta) 
-                        + B(i,j,phi,mi,eta) + B(i,j-1,phi,mi,eta))  ) ;
-
-                if ( phi[i][j] - old_value > 0.0000001 ) 
-                {
-		        should_continue = true;
-	        }
-	    }
-            displayPhi(phi, rows, cols);
-    }
     for(int i = 0; i < rows; ++i)
     {
       delete [] phi[i];
