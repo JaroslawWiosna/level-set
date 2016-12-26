03942bb6718f761f14283ac7a8499fbc809f70cb
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2016-12-26 14:31:20 +0100

    LICENCE headers

--- FlagParser/FlagParser.cpp
+++ FlagParser/FlagParser.cpp
@@ -1,3 +1,14 @@
+/**
+ * \class FlagParser
+ *
+ * @par Licence
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+
 #include "FlagParser.hpp"
 #include <cstdio>
 
--- Image/Image.cpp
+++ Image/Image.cpp
@@ -1,3 +1,14 @@
+/**
+ * \class Image
+ *
+ * @par Licence
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+
 #include "Image.hpp"
 #include <cstdio>
 #include <opencv2/opencv.hpp>
