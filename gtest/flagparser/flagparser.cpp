00c6900446b88cf31ca8b3f10682a3d87b8f47dd
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2016-12-26 15:07:09 +0100

    Test for FlagParser

---
+++ gtest/flagparser/flagparser.cpp
@@ -0,0 +1,41 @@
+/**
+ * @par Licence
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+
+#include <cstdio>
+#include <opencv2/opencv.hpp>
+#include <gtest/gtest.h>
+#include "FlagParser.hpp"
+
+TEST(Test, flagparser01)
+{
+	int argct = 3;
+	
+	//char * argvt [3];
+       	//argvt[0] = "level-set";
+	//argvt[1] = "-i";
+	//argvt[2] = "my/path/to/file.jpg";
+
+	const char * argvt [] = {"level-set", "-i", "my/path/to/file.jpg"};
+
+	FlagParser parser(argct, argvt);
+
+	//parser.printFlags();
+	
+	auto flags = parser.getFlags();
+
+	ASSERT_EQ(flags["-i"], "my/path/to/file.jpg");
+	ASSERT_TRUE(true);
+}
+
+int main(int argc, char* argv[])
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
